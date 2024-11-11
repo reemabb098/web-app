1136c5c8de889f9f4cb7e26a2bf27a71fb4bcb25
reemabb098 <reemabanerjee556@gmail.com>
2024-11-11 17:50:43 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+
+st.write("""
+# Heart disease Prediction App
+
+This app predicts "1" if a patient has a heart disease
+""")
+
+st.sidebar.header('User Input Features')
+
+
+def user_input_features():
+    age = st.sidebar.number_input('Enter your age: ')
+
+    sex  = st.sidebar.selectbox('Sex',(0,1))
+    cp = st.sidebar.selectbox('Chest pain type',(0,1,2,3))
+    tres = st.sidebar.number_input('Resting blood pressure: ')
+    chol = st.sidebar.number_input('Serum cholestoral in mg/dl: ')
+    fbs = st.sidebar.selectbox('Fasting blood sugar',(0,1))
+    res = st.sidebar.number_input('Resting electrocardiographic results: ')
+    tha = st.sidebar.number_input('Maximum heart rate achieved: ')
+    exa = st.sidebar.selectbox('Exercise induced angina: ',(0,1))
+    old = st.sidebar.number_input('oldpeak ')
+    slope = st.sidebar.number_input('he slope of the peak exercise ST segmen: ')
+    ca = st.sidebar.selectbox('number of major vessels',(0,1,2,3))
+    thal = st.sidebar.selectbox('thal',(0,1,2,3))
+
+    data = {'age': age,
+            'sex': sex, 
+            'cp': cp,
+            'trestbps':tres,
+            'chol': chol,
+            'fbs': fbs,
+            'restecg': res,
+            'thalach':tha,
+            'exang':exa,
+            'oldpeak':old,
+            'slope':slope,
+            'ca':ca,
+            'thal':thal
+                }
+    features = pd.DataFrame(data, index=[0])
+    return features
+input_df = user_input_features()
+
+heart_dataset = pd.read_csv('heart.csv')
+heart_dataset = heart_dataset.drop(columns=['target'])
+
+df = pd.concat([input_df,heart_dataset],axis=0)
+
+df = pd.get_dummies(df, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
+df.columns = df.columns.str.replace('.0', '', regex=False)
+
+
+df = df[:1] 
+
+st.write(input_df)
+# Reads in saved classification model
+
+load_clf = pickle.load(open('random_forest_model.pkl', 'rb'))
+
+
+
+
+# Apply model to make predictions
+prediction = load_clf.predict(df)
+prediction_proba = load_clf.predict_proba(df)
+
+
+st.subheader('Prediction')
+st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
